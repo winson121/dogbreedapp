7e2844c685fa1ecf15f4a2f40762013e37151713
Winson <winsonwijaya45@gmail.com>
2023-02-10 15:53:55 +0700

    add the starter file from springboot initialzr

---
+++ src/main/java/com/winson121/springboot/demo/dogbreedapp/DogbreedappApplication.java
@@ -0,0 +1,13 @@
+package com.winson121.springboot.demo.dogbreedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogbreedappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogbreedappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winson121/springboot/demo/dogbreedapp/DogbreedappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winson121.springboot.demo.dogbreedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogbreedappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
